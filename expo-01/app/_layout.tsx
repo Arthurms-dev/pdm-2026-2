2eef93b07291930b9ef440a1608b14b9cb507634
Arthur <arthurms707@gmail.com>
2026-09-14 17:14:41 -0300

    Expo-01 completo

--- expo-01/app/_layout.tsx
+++ expo-01/app/_layout.tsx
@@ -1,5 +1,11 @@
+import Providers from "@/Providers";
 import { Stack } from "expo-router";
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Providers>
+      <Stack />
+    </Providers>
+  );
 }
